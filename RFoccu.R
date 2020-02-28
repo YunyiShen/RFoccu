a4a0d192484cb062350cda465aba1f69e10fbc8f
YunyiShen <yshen99@wisc.edu>
2020-02-28 16:43:04 -0600

    init commit

---
+++ RFoccu.R
@@ -0,0 +1,76 @@
+RFoccu = function(y, occu_x, det_x, occu_x_new=NULL, 
+                  burn_in=1000,n_sample = 10000,thin_by=10,...){
+  # @y: a detection histroy matrix, row as site and col as period, no NA for now
+  # @x_occu x_det: 
+  #  design matrix for occupancy and detection, detection part should be a list
+  # @x_new: new design matrix to project on, occupancy only for now
+  # @burn_in burn in 
+  # @n_sample: how many posterior sample needed
+  # @thin_by: thinning
+  # @...: ... for randomforest
+  require(randomForest)
+  require(coda)
+  
+  psi = mcmc(matrix(NA,floor(n_sample/thin_by),nrow(y)),thin = thin_by)
+  Z = psi
+  if(!is.null(occu_x_new)) psi_new = mcmc(matrix(NA,floor(n_sample/thin_by),nrow(x_new)),thin = thin_by)
+  p = mcmc(matrix(NA,floor(n_sample/thin_by),nrow(y)*ncol(y)),thin = thin_by)
+  
+  Z_curr = 1 * (rowSums(y)>0)
+  p_curr_mat = y
+  psi_curr = Z_curr
+  Z_abs = Z_curr
+  det_x_long = Reduce(rbind,det_x)
+  k = 1
+  for (i in 1:(burn_in+n_sample)){
+    svMisc::progress(((i-1)/(n_sample + burn_in))*100,progress.bar = T)
+    # find psi:
+    RF_occu_curr = randomForest(x=occu_x,y=Z_curr,...)
+    psi_curr = predict(RF_occu_curr,newdata = occu_x)
+    psi_curr = psi_curr * (psi_curr>=0)
+    if(!is.null(occu_x_new)) psi_new_curr = predict(RF_occu_curr,newdata = occu_x_new)
+    
+    
+    
+    # now, find posterior of p:
+    red_det_x = lapply(det_x,function(w,Z_curr){
+      w[Z_curr==1,]
+    },Z_curr) # only those site occupied was considered in training
+    
+    red_det_his = y[Z_curr==1,] # only get the detection history when Z=1 (red for reduced)
+    red_det_x_long = Reduce(rbind,red_det_x) # get the long version design matrix
+
+    red_det_his_long = matrix((red_det_his),length(red_det_his)) # long response 
+    # is by period i.e. first row was site1 period1 and second row was site 2 period 1
+    rm(red_det_x,red_det_his)
+    # fit detection RF:
+    RF_det_curr = randomForest(x=red_det_x_long,y=red_det_his_long,...)
+    
+    # current detection probability estimation:
+    p_curr = predict(RF_det_curr,newdata = det_x_long)
+    p_curr_mat = (matrix(p_curr,ncol = ncol(y)))  # matrix version 
+    
+    #likelihood of seeing the detection history
+    joint_p_curr_mat = exp(rowSums(y*log(p_curr_mat+1e-12)+(1-y)*log(1-p_curr_mat+1e-12)))
+    
+    # now sample posterior Z
+    ## remember this later one only work when no detection at such site
+    psi_posterior=joint_p_curr_mat*psi_curr/
+                  ((1-psi_curr)+joint_p_curr_mat*psi_curr)
+    Z_temp = runif(nrow(y))<psi_posterior
+    Z_curr[Z_abs==0] = Z_temp[Z_abs==0]
+    
+    
+    # saving current sample
+    if(i>burn_in & (i-burn_in)%% thin_by==0) {
+      p[k,] = p_curr
+      psi[k,]=psi_curr
+      Z[k,]=Z_curr
+      if(!is.null(occu_x_new)) psi_new[k,]=psi_new_curr
+      k=k+1
+    }
+  }
+  
+  if(is.null(occu_x_new)) psi_new=NULL
+  list(psi=psi, p=p, Z=Z, psi_new=psi_new)
+}
---
+++ test.R
@@ -0,0 +1,27 @@
+source("RFoccu.R")
+
+
+set.seed(42)
+nsite = 500
+occu_x = rnorm(nsite)
+Z = occu_x>=-0.5 & occu_x<=0.8
+
+nperiod = 3
+
+det_x = list()
+dethist = matrix(NA,nsite,nperiod)
+
+for(i in 1:nperiod){
+  det_x_temp=rnorm(nsite)
+  
+  dethist[,i] = (det_x_temp<=-.5) * Z 
+  
+  det_x[[i]] = cbind(occu_x,det_x_temp)
+}
+
+
+test_RFoccu = RFoccu(y=dethist, matrix(occu_x), det_x, occu_x_new=NULL, 
+                  burn_in=1000,n_sample = 10000,thin_by=1)
+  
+
+
